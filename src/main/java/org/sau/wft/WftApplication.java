1fc737c4194eea2712d676f80d9d95d9886c2fe4
Ahmet Özmen <ozmen@sakarya.edu.tr>
2024-04-24 16:30:00 +0300

    Initial commit

---
+++ src/main/java/org/sau/wft/WftApplication.java
@@ -0,0 +1,13 @@
+package org.sau.wft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sau/wft/WftApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sau.wft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
